f28157774cc27876022ae0e69f1b2c94967df961
heidotrofimov <69143028+heidotrofimov@users.noreply.github.com>
2021-03-29 14:42:27 +0300

    Update download.py

--- download.py
+++ download.py
@@ -3,13 +3,27 @@ import cdsapi
 c = cdsapi.Client()
 
 c.retrieve(
-    'reanalysis-era5-land',
+    'reanalysis-era5-single-levels',
     {
+        'product_type': 'reanalysis',
         'format': 'netcdf',
-        'variable': '2m_dewpoint_temperature',
         'year': '2021',
         'month': '03',
-        'day': '01',
-        'time': '00:00',
+        'variable': '2m_dewpoint_temperature',
+        'day': '24',
+        'time': [
+            '00:00', '01:00', '02:00',
+            '03:00', '04:00', '05:00',
+            '06:00', '07:00', '08:00',
+            '09:00', '10:00', '11:00',
+            '12:00', '13:00', '14:00',
+            '15:00', '16:00', '17:00',
+            '18:00', '19:00', '20:00',
+            '21:00',
+        ],
+        'area': [
+            58.29, 26.48, 58.27,
+            26.5,
+        ],
     },
     'download.nc')
